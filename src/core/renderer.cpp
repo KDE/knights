f2cee6dcca3bf280f5fdd39ccdf31fdb673b2fc7
Miha Čančula <miha.cancula@gmail.com>
2010-08-17 13:15:48 +0000

    Added missing files renderer.h and renderer.cpp
    svn path=/trunk/playground/games/knights/; revision=1164683

---
+++ src/core/renderer.cpp
@@ -0,0 +1,62 @@
+/*
+  This file is part of Knights, a chess board for KDE SC 4.
+  Copyright 2009-2010  Miha Čančula <miha.cancula@gmail.com>
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License as
+  published by the Free Software Foundation; either version 2 of
+  the License or (at your option) version 3 or any later version
+  accepted by the membership of KDE e.V. (or its successor approved
+  by the membership of KDE e.V.), which shall act as a proxy
+  defined in Section 14 of version 3 of the license.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "renderer.h"
+
+#if not defined HAVE_RENDER
+#include <KGameTheme>
+
+using namespace Knights;
+
+Renderer::Renderer(const QString& defaultTheme)
+{
+    m_theme = new KGameTheme;
+    setTheme(defaultTheme);
+}
+
+Renderer::~Renderer()
+{
+    delete m_theme;
+}
+
+bool Renderer::spriteExists(const QString& key)
+{
+    return elementExists(key);
+}
+
+QRectF Renderer::boundsOnSprite(const QString& key)
+{
+    return boundsOnElement(key);
+}
+
+void Renderer::setTheme(const QString& theme)
+{
+    if (!m_theme->load(theme))
+    {
+        m_theme->loadDefault();
+    }
+    load(m_theme->graphics());
+}
+
+
+
+
+#endif // HAVE_RENDER
---
+++ src/core/renderer.h
@@ -0,0 +1,61 @@
+/*
+  This file is part of Knights, a chess board for KDE SC 4.
+  Copyright 2009-2010  Miha Čančula <miha.cancula@gmail.com>
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License as
+  published by the Free Software Foundation; either version 2 of
+  the License or (at your option) version 3 or any later version
+  accepted by the membership of KDE e.V. (or its successor approved
+  by the membership of KDE e.V.), which shall act as a proxy
+  defined in Section 14 of version 3 of the license.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef KNIGHTS_RENDERER_H
+#define KNIGHTS_RENDERER_H
+
+#include "kdeversion.h"
+#if KDE_IS_VERSION(4,5,60)
+    #define HAVE_RENDER
+#endif
+
+#if not defined HAVE_RENDER
+    #include <QtSvg/QSvgRenderer>
+#endif
+
+class KGameTheme;
+
+namespace Knights {
+
+#if defined HAVE_RENDER
+typedef KGameRenderer Renderer;
+#else
+class Renderer : public QSvgRenderer
+{
+    Q_OBJECT
+    public:
+    Renderer(const QString& defaultTheme);
+    virtual ~Renderer();
+    
+    bool spriteExists(const QString& key);
+    QRectF boundsOnSprite(const QString& key);
+    
+    public Q_SLOTS:
+    void setTheme(const QString& theme);
+    
+    private:
+        KGameTheme* m_theme;
+};
+#endif
+
+}
+
+#endif // KNIGHTS_RENDERER_H
