c9e1791c27cbf460d77770557f73970c361248f2
Laurent Montel <montel@kde.org>
2010-09-23 20:38:27 +0000

    Fix extract message
    svn path=/trunk/kdereview/knights/; revision=1178760

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 $EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
-$XGETTEXT *.cpp -o $podir/knights.pot
+$XGETTEXT proto/*.cpp *.cpp -o $podir/knights.pot
 rm -f rc.cpp
