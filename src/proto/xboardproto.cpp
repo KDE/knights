27a92b8367ce3e77791cd0fff85463da1dc7ea6b
Miha Čančula <miha.cancula@gmail.com>
2011-02-15 20:30:54 +0100

    Go into force mode before undoing moves.

--- src/proto/xboardproto.cpp
+++ src/proto/xboardproto.cpp
@@ -245,9 +245,20 @@ void XBoardProtocol::acceptOffer(const Offer& offer)
             break;
             
         case ActionUndo:
+            write ( "force" );
             for ( int i = 0; i < offer.numberOfMoves; ++i )
             {
                 write ( "undo" );
+            }
+            // This function is called before changeActivePlayer, so we must take into accont 
+            // the number of moves being undone. 
+            if ( ( Manager::self()->activePlayer() == color() ) == ( ( offer.numberOfMoves % 2 ) == 0 ) )
+            {
+                write ( "go" );
+            }
+            else
+            {
+                resumePending = true;
             }
             break;
             
