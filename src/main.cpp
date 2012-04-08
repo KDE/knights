468c244b512317b8aacf0da45708ae861f521549
Miha Čančula <miha@noughmad.eu>
2012-04-08 19:27:37 +0200

    Bump the version

--- src/main.cpp
+++ src/main.cpp
@@ -29,7 +29,7 @@
 static const char description[] =
     I18N_NOOP ( "Chess board based on KDE Development Platform 4" );
 
-static const char version[] = "2.4.1";
+static const char version[] = "2.4.2";
 
 int main ( int argc, char **argv )
 {
