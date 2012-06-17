9ca3473ebeb7d3fcddc025c98c58e1f686bffc29
Miha Čančula <miha@noughmad.eu>
2012-06-17 19:53:35 +0200

    Bump the version to show breaking compatibility with KDE < 4.9

--- src/main.cpp
+++ src/main.cpp
@@ -29,7 +29,7 @@
 static const char description[] =
     I18N_NOOP ( "Chess board based on KDE Development Platform 4" );
 
-static const char version[] = "2.4.2";
+static const char version[] = "2.4.90 (2.5 Beta 1)";
 
 int main ( int argc, char **argv )
 {
