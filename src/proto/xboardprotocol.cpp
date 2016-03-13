2f92ba88ac66baeaf1865b989dde03b556144cae
Alexander Semke <alexander.semke@web.de>
2016-03-13 11:21:06 +0100

    Fixed incorrect pawn promotion in the Xboard protocol BUG: 333256
    Code cleanup in XBoardProtocol.

--- src/proto/xboardprotocol.cpp
+++ src/proto/xboardprotocol.cpp
@@ -1,355 +1,282 @@
-/*
-    This file is part of Knights, a chess board for KDE SC 4.
-    Copyright 2009,2010,2011  Miha Čančula <miha@noughmad.eu>
+/***************************************************************************
+    File                 : xboardprotocol.cpp
+    Project              : Knights
+    Description          : Wrapper for the XBoard protocol
+    --------------------------------------------------------------------
+    Copyright            : (C) 2016 by Alexander Semke (alexander.semke@web.de)
+    Copyright            : (C) 2009-2011 by Miha Čančula (miha@noughmad.eu)
 
-    This program is free software; you can redistribute it and/or
-    modify it under the terms of the GNU General Public License as
-    published by the Free Software Foundation; either version 2 of
-    the License or (at your option) version 3 or any later version
-    accepted by the membership of KDE e.V. (or its successor approved
-    by the membership of KDE e.V.), which shall act as a proxy
-    defined in Section 14 of version 3 of the license.
+ ***************************************************************************/
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
-#include <settings.h>
+/***************************************************************************
+ *                                                                         *
+ *  This program is free software; you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation; either version 2 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the Free Software           *
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor,                    *
+ *   Boston, MA  02110-1301  USA                                           *
+ *                                                                         *
+ ***************************************************************************/
 #include "proto/xboardprotocol.h"
-#include "proto/chatwidget.h"
 #include "gamemanager.h"
 #include "knightsdebug.h"
 
 #include <KProcess>
-#include <KLocalizedString>
-
 #include <QFileDialog>
 
 using namespace Knights;
 
-XBoardProtocol::XBoardProtocol ( QObject* parent ) : ComputerProtocol ( parent )
-, m_moves(0)
-, m_increment(0)
-, m_baseTime(0)
-, m_timeLimit(0)
-{
+XBoardProtocol::XBoardProtocol(QObject* parent) : ComputerProtocol(parent),
+	m_resumePending(false),
+	m_moves(0),
+	m_increment(0),
+	m_baseTime(0),
+	m_timeLimit(0) {
 
 }
 
-Protocol::Features XBoardProtocol::supportedFeatures()
-{
-    // FIXME: This shouldn't be hardcoded. For instance, a chess engine which
-    // supports the XBoard protocol may or may not support the pause action; we
-    // should find this out using the 'protover' command which will reply with
-    // the 'feature' command. See the XBoard protocol specification:
-    // http://home.hccnet.nl/h.g.muller/engine-intf.html
+Protocol::Features XBoardProtocol::supportedFeatures() {
+	// FIXME: This shouldn't be hardcoded. For instance, a chess engine which
+	// supports the XBoard protocol may or may not support the pause action; we
+	// should find this out using the 'protover' command which will reply with
+	// the 'feature' command. See the XBoard protocol specification:
+	// http://home.hccnet.nl/h.g.muller/engine-intf.html
 
-    //return GameOver | Pause | Draw | Adjourn | Resign | Undo | SetDifficulty | AdjustDifficulty;
-    return GameOver | Pause | Draw | Resign | Undo | SetDifficulty | AdjustDifficulty;
+	//return GameOver | Pause | Draw | Adjourn | Resign | Undo | SetDifficulty | AdjustDifficulty;
+	return GameOver | Pause | Draw | Resign | Undo | SetDifficulty | AdjustDifficulty;
 }
 
-XBoardProtocol::~XBoardProtocol()
-{
-    if ( mProcess && mProcess->isOpen() )
-    {
-        write("quit");
-        if ( !mProcess->waitForFinished ( 500 ) )
-        {
-            mProcess->kill();
-        }
-    }
+XBoardProtocol::~XBoardProtocol() {
+	if ( mProcess && mProcess->isOpen() ) {
+		write("quit");
+		if ( !mProcess->waitForFinished ( 500 ) )
+			mProcess->kill();
+	}
 }
 
-void XBoardProtocol::startGame()
-{
-    qCDebug(LOG_KNIGHTS) << colorName(color());
-    TimeControl c = Manager::self()->timeControl ( White );
-    if ( c.baseTime != QTime() )
-    {
-        write(QString(QLatin1String("level %1 %2 %3")).arg(c.moves).arg(QTime().secsTo(c.baseTime)/60).arg(c.increment));
-    }
-    if ( color() == White )
-    {
-        write("go");
-    }
-    resumePending = false;
+void XBoardProtocol::startGame() {
+	qCDebug(LOG_KNIGHTS) << colorName(color());
+	TimeControl c = Manager::self()->timeControl(White);
+	if ( c.baseTime != QTime() )
+		write(QString(QLatin1String("level %1 %2 %3")).arg(c.moves).arg(QTime().secsTo(c.baseTime)/60).arg(c.increment));
+
+	if (color() == White)
+		write("go");
+
+	m_resumePending = false;
 }
 
-void XBoardProtocol::move ( const Move& m )
-{
-    qCDebug(LOG_KNIGHTS) << "Player's move:" << m.string(false);
-    write ( m.string(false) );
-    lastMoveString.clear();
-    emit undoPossible ( false );
-    if ( resumePending )
-    {
-        write("go");
-        resumePending = false;
-    }
+void XBoardProtocol::move ( const Move& m ) {
+	QString str = m.string(false);
+	if (m.promotedType())
+		str = str.toLower(); // "e7e8q" is used for the pawn promotion -> convert Q in the move string to lowercase q
+
+	qCDebug(LOG_KNIGHTS) << "Player's move:" << str;
+	write(str);
+
+	lastMoveString.clear();
+	emit undoPossible ( false );
+	if ( m_resumePending ) {
+		write("go");
+		m_resumePending = false;
+	}
 }
 
-void XBoardProtocol::init (  )
-{
-    startProgram();
-    write("xboard");
-    initComplete();
+void XBoardProtocol::init (  ) {
+	startProgram();
+	write("xboard");
+	initComplete();
 }
 
-QList< Protocol::ToolWidgetData > XBoardProtocol::toolWidgets()
-{
-    return ComputerProtocol::toolWidgets();
+QList< Protocol::ToolWidgetData > XBoardProtocol::toolWidgets() {
+	return ComputerProtocol::toolWidgets();
 }
 
-bool XBoardProtocol::parseStub(const QString& line)
-{
-    parseLine(line);
-    return true;
+bool XBoardProtocol::parseStub(const QString& line) {
+	parseLine(line);
+	return true;
 }
 
-bool XBoardProtocol::parseLine(const QString& line)
-{
-        if ( line.isEmpty() )
-        {
-            return true;
-        }
-        bool display = true;
-        ChatWidget::MessageType type = ChatWidget::GeneralMessage;
-        if ( line.contains ( QLatin1String ( "Illegal move" ) ) )
-        {
-            type = ChatWidget::ErrorMessage;
-            emit illegalMove();
-        }
-        else if ( line.contains ( QLatin1String ( "..." ) ) || line.contains(QLatin1String("move")) )
-        {
-            type = ChatWidget::MoveMessage;
-            const QRegExp position(QLatin1String("[a-h][1-8]"));
-            QString moveString = line.split ( QLatin1Char ( ' ' ) ).last();
-            if ( moveString == lastMoveString )
-            {
-                return true;
-            }
-            lastMoveString = moveString;
-            Move m;
-            if ( position.indexIn(line) > -1 )
-            {
-                m.setString(moveString);
-            }
-            else if ( moveString.contains(QLatin1String("O-O-O"))
-                    || moveString.contains(QLatin1String("o-o-o"))
-                    || moveString.contains(QLatin1String("0-0-0")) )
-            {
-                m = Move::castling(Move::QueenSide, Manager::self()->activePlayer());
-            }
-            else if ( moveString.contains(QLatin1String("O-O"))
-                    || moveString.contains(QLatin1String("o-o"))
-                    || moveString.contains(QLatin1String("0-0")) )
-            {
-                m = Move::castling(Move::KingSide, Manager::self()->activePlayer());
-            }
-            else
-            {
-                type = ChatWidget::GeneralMessage;
-            }
-            if ( m.isValid() )
-            {
-                qCDebug(LOG_KNIGHTS) << "Move by" << attribute("program").toString() << ":" << moveString << "=>" << m;
-                emit pieceMoved ( m );
-                emit undoPossible ( true );
-            }
-        }
-        else if ( line.contains ( QLatin1String ( "wins" ) ) )
-        {
-            type = ChatWidget::StatusMessage;
-            Color winner;
-            if ( line.split ( QLatin1Char ( ' ' ) ).last().contains ( QLatin1String ( "white" ) ) )
-            {
-                winner = White;
-            }
-            else
-            {
-                winner = Black;
-            }
-            emit gameOver ( winner );
-            return true;
-        }
-        else if ( line.contains ( QLatin1String("offer") ) && line.contains ( QLatin1String("draw") ) )
-        {
-            display = false;
-            Offer o;
-            o.action = ActionDraw;
-            o.id = 0;
-            o.player = color();
-            Manager::self()->sendOffer(o);
-        }
-        else if ( line.startsWith ( QLatin1String("1-0") ) )
-        {
-            emit gameOver ( White );
-        }
-        else if ( line.startsWith ( QLatin1String("0-1") ) )
-        {
-            emit gameOver ( NoColor );
-        }
-        else if ( line.startsWith ( QLatin1String("1/2-1/2") ) )
-        {
-            emit gameOver ( Black );
-        }
-        if ( display )
-        {
-            writeToConsole ( line, type );
-        }
-        return true;
-    }
-
-void XBoardProtocol::acceptOffer(const Offer& offer)
-{
-    qCDebug(LOG_KNIGHTS) << "Accepting offer" << offer.text;
-    switch ( offer.action )
-    {
-        case ActionDraw:
-            setWinner(NoColor);
-            break;
-            
-        case ActionAdjourn:
-            write( QLatin1String("save ") + QFileDialog::getSaveFileName() );
-            break;
-            
-        case ActionUndo:
-            for ( int i = 0; i < offer.numberOfMoves/2; ++i )
-            {
-                write ( "remove" );
-            }
-            if (offer.numberOfMoves % 2)
-            {
-                write ( "force" );
-                write ( "undo" );
-                
-                if ( Manager::self()->activePlayer() != color() )
-                {
-                    write ( "go" );
-                }
-                else
-                {
-                    resumePending = true;
-                }
-            }
-            break;
-            
-        case ActionPause:
-            write ( "force" );
-            break;
-            
-        case ActionResume:
-            if ( Manager::self()->activePlayer() == color() )
-            {
-                write ( "go" );
-            }
-            else
-            {
-                resumePending = true;
-            }
-            break;
-            
-        default:
-            qCCritical(LOG_KNIGHTS) << "XBoard should not send this kind offers";
-            break;
-    }
+bool XBoardProtocol::parseLine(const QString& line) {
+	if ( line.isEmpty() )
+		return true;
+	bool display = true;
+	ChatWidget::MessageType type = ChatWidget::GeneralMessage;
+	if ( line.contains ( QLatin1String ( "Illegal move" ) ) ) {
+		type = ChatWidget::ErrorMessage;
+		emit illegalMove();
+	} else if ( line.contains ( QLatin1String ( "..." ) ) || line.contains(QLatin1String("move")) ) {
+		type = ChatWidget::MoveMessage;
+		const QRegExp position(QLatin1String("[a-h][1-8]"));
+		QString moveString = line.split ( QLatin1Char ( ' ' ) ).last();
+		if ( moveString == lastMoveString )
+			return true;
+		lastMoveString = moveString;
+		Move m;
+		if ( position.indexIn(line) > -1 )
+			m.setString(moveString);
+		else if ( moveString.contains(QLatin1String("O-O-O"))
+		          || moveString.contains(QLatin1String("o-o-o"))
+		          || moveString.contains(QLatin1String("0-0-0")) )
+			m = Move::castling(Move::QueenSide, Manager::self()->activePlayer());
+		else if ( moveString.contains(QLatin1String("O-O"))
+		          || moveString.contains(QLatin1String("o-o"))
+		          || moveString.contains(QLatin1String("0-0")) )
+			m = Move::castling(Move::KingSide, Manager::self()->activePlayer());
+		else
+			type = ChatWidget::GeneralMessage;
+		if ( m.isValid() ) {
+			qCDebug(LOG_KNIGHTS) << "Move by" << attribute("program").toString() << ":" << moveString << "=>" << m;
+			emit pieceMoved ( m );
+			emit undoPossible ( true );
+		}
+	} else if ( line.contains ( QLatin1String ( "wins" ) ) ) {
+		type = ChatWidget::StatusMessage;
+		Color winner;
+		if ( line.split ( QLatin1Char ( ' ' ) ).last().contains ( QLatin1String ( "white" ) ) )
+			winner = White;
+		else
+			winner = Black;
+		emit gameOver ( winner );
+		return true;
+	} else if ( line.contains ( QLatin1String("offer") ) && line.contains ( QLatin1String("draw") ) ) {
+		display = false;
+		Offer o;
+		o.action = ActionDraw;
+		o.id = 0;
+		o.player = color();
+		Manager::self()->sendOffer(o);
+	} else if ( line.startsWith ( QLatin1String("1-0") ) )
+		emit gameOver ( White );
+	else if ( line.startsWith ( QLatin1String("0-1") ) )
+		emit gameOver ( NoColor );
+	else if ( line.startsWith ( QLatin1String("1/2-1/2") ) )
+		emit gameOver ( Black );
+	if ( display )
+		writeToConsole ( line, type );
+	return true;
 }
 
-void XBoardProtocol::declineOffer(const Offer& offer)
-{
-    // No special action to do here, ignoring an offer is the same as declining. 
-    Q_UNUSED(offer);
+void XBoardProtocol::acceptOffer(const Offer& offer) {
+	qCDebug(LOG_KNIGHTS) << "Accepting offer" << offer.text;
+	switch ( offer.action ) {
+		case ActionDraw:
+			setWinner(NoColor);
+			break;
+
+		case ActionAdjourn:
+			write( QLatin1String("save ") + QFileDialog::getSaveFileName() );
+			break;
+
+		case ActionUndo:
+			for ( int i = 0; i < offer.numberOfMoves/2; ++i )
+				write ( "remove" );
+			if (offer.numberOfMoves % 2) {
+				write ( "force" );
+				write ( "undo" );
+
+				if ( Manager::self()->activePlayer() != color() )
+					write ( "go" );
+				else
+					m_resumePending = true;
+			}
+			break;
+
+		case ActionPause:
+			write ( "force" );
+			break;
+
+		case ActionResume:
+			if ( Manager::self()->activePlayer() == color() )
+				write ( "go" );
+			else
+				m_resumePending = true;
+			break;
+
+		default:
+			qCCritical(LOG_KNIGHTS) << "XBoard should not send this kind offers";
+			break;
+	}
 }
 
-void XBoardProtocol::setWinner(Color winner)
-{
-    QByteArray result = "result ";
-    switch ( winner )
-    {
-        case White:
-            result += "1-0";
-            break;
-        case Black:
-            result += "0-1";
-            break;
-        case NoColor:
-            result += "1/2-1/2";
-            break;
-    }
-    write(QLatin1String(result));
+void XBoardProtocol::declineOffer(const Offer& offer) {
+	// No special action to do here, ignoring an offer is the same as declining.
+	Q_UNUSED(offer);
 }
 
-void XBoardProtocol::makeOffer(const Offer& offer)
-{
-    switch ( offer.action )
-    {
-        case ActionDraw:
-            write("draw");
-            break;
-            
-        case ActionAdjourn:
-            write( QLatin1String("save ") + QFileDialog::getSaveFileName() );
-            offer.accept();
-            break;
-            
-        case ActionUndo:
-            for ( int i = 0; i < offer.numberOfMoves/2; ++i )
-            {
-                write ( "remove" );
-            }
-            if (offer.numberOfMoves % 2)
-            {
-                write ( "force" );
-                write ( "undo" );
-                
-                if ( Manager::self()->activePlayer() != color() )
-                {
-                    write ( "go" );
-                }
-                else
-                {
-                    resumePending = true;
-                }
-            }
-            offer.accept();
-            break;
-            
-        case ActionPause:
-            write ( "force" );
-            offer.accept();
-            break;
-            
-        case ActionResume:
-            if ( Manager::self()->activePlayer() == color() )
-            {
-                write ( "go" );
-            }
-            else
-            {
-                resumePending = true;
-            }
-            offer.accept();
-            break;
-            
-        default:
-            break;
-    }
+void XBoardProtocol::setWinner(Color winner) {
+	QByteArray result = "result ";
+	switch ( winner ) {
+		case White:
+			result += "1-0";
+			break;
+		case Black:
+			result += "0-1";
+			break;
+		case NoColor:
+			result += "1/2-1/2";
+			break;
+	}
+	write(QLatin1String(result));
 }
 
-void XBoardProtocol::setDifficulty(int depth, int memory)
-{
-    // Gnuchess only supports 'depth', while Crafty (and the XBoard protocol) wants 'sd'. 
-    // So we give both. 
-    write ( QLatin1String("depth ") + QString::number ( depth ) );
-    write ( QLatin1String("sd ") + QString::number ( depth ) );
-    write ( QLatin1String("memory ") + QString::number ( memory ) );
+void XBoardProtocol::makeOffer(const Offer& offer) {
+	switch ( offer.action ) {
+		case ActionDraw:
+			write("draw");
+			break;
+
+		case ActionAdjourn:
+			write( QLatin1String("save ") + QFileDialog::getSaveFileName() );
+			offer.accept();
+			break;
+
+		case ActionUndo:
+			for ( int i = 0; i < offer.numberOfMoves/2; ++i )
+				write ( "remove" );
+			if (offer.numberOfMoves % 2) {
+				write ( "force" );
+				write ( "undo" );
+
+				if ( Manager::self()->activePlayer() != color() )
+					write ( "go" );
+				else
+					m_resumePending = true;
+			}
+			offer.accept();
+			break;
+
+		case ActionPause:
+			write ( "force" );
+			offer.accept();
+			break;
+
+		case ActionResume:
+			if ( Manager::self()->activePlayer() == color() )
+				write ( "go" );
+			else
+				m_resumePending = true;
+			offer.accept();
+			break;
+
+		default:
+			break;
+	}
 }
 
-
-
-// kate: indent-mode cstyle; space-indent on; indent-width 4; replace-tabs on;  replace-tabs on;  replace-tabs on;  replace-tabs on;
+void XBoardProtocol::setDifficulty(int depth, int memory) {
+	// Gnuchess only supports 'depth', while Crafty (and the XBoard protocol) wants 'sd'.
+	// So we give both.
+	write ( QLatin1String("depth ") + QString::number ( depth ) );
+	write ( QLatin1String("sd ") + QString::number ( depth ) );
+	write ( QLatin1String("memory ") + QString::number ( memory ) );
+}
--- src/proto/xboardprotocol.h
+++ src/proto/xboardprotocol.h
@@ -1,69 +1,73 @@
-/*
-    This file is part of Knights, a chess board for KDE SC 4.
-    Copyright 2009,2010,2011  Miha Čančula <miha@noughmad.eu>
+/***************************************************************************
+    File                 : xboardprotocol.h
+    Project              : Knights
+    Description          : Wrapper for the XBoard protocol
+    --------------------------------------------------------------------
+    Copyright            : (C) 2016 by Alexander Semke (alexander.semke@web.de)
+    Copyright            : (C) 2009-2011 by Miha Čančula (miha@noughmad.eu)
 
-    This program is free software; you can redistribute it and/or
-    modify it under the terms of the GNU General Public License as
-    published by the Free Software Foundation; either version 2 of
-    the License or (at your option) version 3 or any later version
-    accepted by the membership of KDE e.V. (or its successor approved
-    by the membership of KDE e.V.), which shall act as a proxy
-    defined in Section 14 of version 3 of the license.
+ ***************************************************************************/
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+/***************************************************************************
+ *                                                                         *
+ *  This program is free software; you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation; either version 2 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the Free Software           *
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor,                    *
+ *   Boston, MA  02110-1301  USA                                           *
+ *                                                                         *
+ ***************************************************************************/
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
-#ifndef KNIGHTS_XBOARDPROTOCOL_H
-#define KNIGHTS_XBOARDPROTOCOL_H
+#ifndef XBOARDPROTOCOL_H
+#define XBOARDPROTOCOL_H
 
 #include "proto/computerprotocol.h"
 
-class KProcess;
+namespace Knights {
 
-namespace Knights
-{
-    class XBoardProtocol : public ComputerProtocol
-    {
-            Q_OBJECT
-        public:
-            XBoardProtocol ( QObject* parent = 0 );
-            ~XBoardProtocol();
+class XBoardProtocol : public ComputerProtocol {
+	Q_OBJECT
 
-            virtual void move ( const Move& m );
-            virtual Features supportedFeatures();
+public:
+	XBoardProtocol(QObject* parent = 0);
+	~XBoardProtocol();
 
-    virtual QList<ToolWidgetData> toolWidgets();
+	virtual void move(const Move&);
+	virtual Features supportedFeatures();
 
-    virtual bool parseLine(const QString& line);
-    virtual bool parseStub(const QString& line);
+	virtual QList<ToolWidgetData> toolWidgets();
 
-        private:
-            QString lastMoveString;
-            bool resumePending;
-    int m_moves;
-    int m_increment;
-    int m_baseTime;
-    bool m_timeLimit;
+	virtual bool parseLine(const QString& line);
+	virtual bool parseStub(const QString& line);
 
-        public Q_SLOTS:
-            virtual void init ();
-            virtual void startGame();
-            virtual void setWinner(Color winner);
-            
-    virtual void makeOffer(const Offer& offer);
-    virtual void acceptOffer(const Offer& offer);
-    virtual void declineOffer(const Offer& offer);
-    
-    virtual void setDifficulty(int depth, int memory);
-    };
+private:
+	QString lastMoveString;
+	bool m_resumePending;
+	int m_moves;
+	int m_increment;
+	int m_baseTime;
+	bool m_timeLimit;
+
+public Q_SLOTS:
+	virtual void init ();
+	virtual void startGame();
+	virtual void setWinner(Color);
+
+	virtual void makeOffer(const Offer&);
+	virtual void acceptOffer(const Offer&);
+	virtual void declineOffer(const Offer&);
+
+	virtual void setDifficulty(int depth, int memory);
+};
 }
 
-#endif // KNIGHTS_XBOARDPROTOCOL_H
-
-// kate: indent-mode cstyle; space-indent on; indent-width 4; replace-tabs on;  replace-tabs on;  replace-tabs on;
+#endif //XBOARDPROTOCOL_H
