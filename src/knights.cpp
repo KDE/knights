630583ceb8ca63edd31215b9b6ced69b90e1886c
Alexander Semke <alexander.semke@web.de>
2017-09-09 17:40:46 +0200

    1. updated license headers in knights.h/cpp 2. use range based for loop instead of foreach in knights.cpp

--- src/knights.cpp
+++ src/knights.cpp
@@ -1,24 +1,31 @@
-/*
-    This file is part of Knights, a chess board for KDE SC 4.
-    Copyright 2009,2010,2011  Miha Čančula <miha@noughmad.eu>
-    Copyright 2016 Alexander Semke <alexander.semke@web.de>
+/***************************************************************************
+    File                 : knights.cpp
+    Project              : Knights
+    Description          : Main window of the applicatin
+    --------------------------------------------------------------------
+    Copyright            : (C) 2016-1017 by Alexander Semke (alexander.semke@web.de)
+    Copyright            : (C) 2010-2012 by Miha Čančula (miha@noughmad.eu)
 
-    This program is free software; you can redistribute it and/or
-    modify it under the terms of the GNU General Public License as
-    published by the Free Software Foundation; either version 2 of
-    the License or (at your option) version 3 or any later version
-    accepted by the membership of KDE e.V. (or its successor approved
-    by the membership of KDE e.V.), which shall act as a proxy
-    defined in Section 14 of version 3 of the license.
+ ***************************************************************************/
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+/***************************************************************************
+ *                                                                         *
+ *  This program is free software; you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation; either version 2 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the Free Software           *
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor,                    *
+ *   Boston, MA  02110-1301  USA                                           *
+ *                                                                         *
+ ***************************************************************************/
 
 #include "knights.h"
 #include "proto/localprotocol.h"
@@ -279,21 +286,6 @@ void MainWindow::fileLoad() {
 	m_saveAction->setEnabled(false);
 }
 
-//TODO
-// void MainWindow::showFicsDialog(Color color, bool computer)
-// {
-//     if(computer || true)   // TODO: Implement, and remove this check
-//     {
-//         KMessageBox::sorry(this, i18n("This feature is not yet implemented in Knights"));
-//         QTimer::singleShot(1, this, SLOT(fileNew()));
-//         return;
-//     }
-//     FicsProtocol* p = new FicsProtocol();
-//     p->setColor(color);
-//     p->init();
-//     p->openGameDialog();
-// }
-
 void MainWindow::protocolInitSuccesful() {
 	qCDebug(LOG_KNIGHTS) << "Show Clock:" << Settings::showClock() << "Show Console:" << Settings::showConsole();
 	QString whiteName = Protocol::white()->playerName();
@@ -345,7 +337,7 @@ void MainWindow::protocolInitSuccesful() {
 	QList<Protocol::ToolWidgetData> list;
 	list << Protocol::black()->toolWidgets();
 	list << Protocol::white()->toolWidgets();
-	foreach(const Protocol::ToolWidgetData& data, list) {
+	for (const auto& data : list) {
 		switch(data.type) {
 		case Protocol::ConsoleToolWidget:
 			if(data.owner == White) {
@@ -488,7 +480,6 @@ void MainWindow::setShowClockSetting(bool value) {
 void MainWindow::setShowHistorySetting(bool value) {
 	Settings::setShowHistory(value);
 }
-
 
 void MainWindow::setShowConsoleSetting() {
 	if((actionCollection()->action(QLatin1String("show_console_white"))->isChecked()) && (actionCollection()->action(QLatin1String("show_console_white"))->isVisible()))
--- src/knights.h
+++ src/knights.h
@@ -1,24 +1,31 @@
-/*
-    This file is part of Knights, a chess board for KDE SC 4.
-    Copyright 2009,2010,2011  Miha Čančula <miha@noughmad.eu>
-    Copyright 2016 Alexander Semke <alexander.semke@web.de>
+/***************************************************************************
+    File                 : knights.h
+    Project              : Knights
+    Description          : Main window of the applicatin
+    --------------------------------------------------------------------
+    Copyright            : (C) 2016-1017 by Alexander Semke (alexander.semke@web.de)
+    Copyright            : (C) 2010-2012 by Miha Čančula (miha@noughmad.eu)
 
-    This program is free software; you can redistribute it and/or
-    modify it under the terms of the GNU General Public License as
-    published by the Free Software Foundation; either version 2 of
-    the License or (at your option) version 3 or any later version
-    accepted by the membership of KDE e.V. (or its successor approved
-    by the membership of KDE e.V.), which shall act as a proxy
-    defined in Section 14 of version 3 of the license.
+ ***************************************************************************/
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+/***************************************************************************
+ *                                                                         *
+ *  This program is free software; you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation; either version 2 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the Free Software           *
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor,                    *
+ *   Boston, MA  02110-1301  USA                                           *
+ *                                                                         *
+ ***************************************************************************/
 
 #ifndef KNIGHTS_H
 #define KNIGHTS_H
