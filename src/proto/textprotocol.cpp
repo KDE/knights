8badafb3bcc1596f9622549a3e6c9c48056b2e0b
Alexander Semke <alexander.semke@web.de>
2017-10-03 12:29:28 +0200

    Reformatted textprotocol.h/cpp and added license headers.

--- src/proto/textprotocol.cpp
+++ src/proto/textprotocol.cpp
@@ -1,127 +1,101 @@
-/*
-    This file is part of Knights, a chess board for KDE SC 4.
-    Copyright 2009,2010,2011  Miha Čančula <miha@noughmad.eu>
+/***************************************************************************
+    File                 : textprotocol.cpp
+    Project              : Knights
+    Description          : Base class for text protocols
+    --------------------------------------------------------------------
+    Copyright            : (C) 2017 by Alexander Semke (alexander.semke@web.de)
+    Copyright            : (C) 2009-2011 by Miha Čančula (miha@noughmad.eu)
 
-    This program is free software; you can redistribute it and/or
-    modify it under the terms of the GNU General Public License as
-    published by the Free Software Foundation; either version 2 of
-    the License or (at your option) version 3 or any later version
-    accepted by the membership of KDE e.V. (or its successor approved
-    by the membership of KDE e.V.), which shall act as a proxy
-    defined in Section 14 of version 3 of the license.
+ ***************************************************************************/
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
+/***************************************************************************
+ *                                                                         *
+ *  This program is free software; you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation; either version 2 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the Free Software           *
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor,                    *
+ *   Boston, MA  02110-1301  USA                                           *
+ *                                                                         *
+ ***************************************************************************/
 #include "knightsdebug.h"
 #include "textprotocol.h"
 
 using namespace Knights;
 
-TextProtocol::TextProtocol(QObject* parent): Protocol(parent), m_console(0)
-{
+TextProtocol::TextProtocol(QObject* parent): Protocol(parent), m_console(0) {
 
 }
 
-TextProtocol::~TextProtocol()
-{
+TextProtocol::~TextProtocol() {
 
 }
 
-void TextProtocol::readFromDevice()
-{
-    while ( !stream.atEnd() )
-    {
-	if ( stream.device()->canReadLine() )
-	{
-            const QString text = line + stream.readLine().trimmed();
-	    if (!parseLine(text))
-            {
-                line = text;
-            }
+void TextProtocol::readFromDevice() {
+	while ( !stream.atEnd() ) {
+		if ( stream.device()->canReadLine() ) {
+			const QString text = line + stream.readLine().trimmed();
+			if (!parseLine(text))
+				line = text;
+		} else {
+			const QString all = stream.readAll();
+			foreach ( const QString& newLine, all.split( QLatin1Char('\n') ) ) {
+				const QString text = line + newLine.trimmed();
+				if ( !parseStub(text) && !parseLine(text) )
+					line = text;
+			}
+		}
 	}
+}
+
+void TextProtocol::setDevice(QIODevice* device) {
+	stream.setDevice(device);
+	connect ( device, &QIODevice::readyRead, this, &TextProtocol::readFromDevice );
+}
+
+QIODevice* TextProtocol::device() const {
+	return stream.device();
+}
+
+void TextProtocol::writeCheckMoves(const QString& text) {
+	Move m = Move(text);
+	if ( m.isValid() )
+		emit pieceMoved(m);
+	write(text);
+}
+
+void TextProtocol::write(const QString& text) {
+	qCDebug(LOG_KNIGHTS) << text;
+	stream << text << endl;
+}
+
+void TextProtocol::write(const char* text) {
+	qCDebug(LOG_KNIGHTS) << text;
+	stream << text << endl;
+}
+
+void TextProtocol::setConsole(ChatWidget* widget) {
+	m_console = widget;
+	foreach ( const ChatWidget::Message& message, messages )
+		m_console->addText ( message );
+	messages.clear();
+}
+
+ChatWidget* TextProtocol::console() const {
+	return m_console;
+}
+
+void TextProtocol::writeToConsole(const QString& text, ChatWidget::MessageType type) {
+	if ( m_console )
+		m_console->addText ( text, type );
 	else
-	{
-	    foreach ( const QString& newLine, stream.readAll().split( QLatin1Char('\n') ) )
-	    {
-                const QString text = line + newLine.trimmed();
-                if ( !parseStub(text) && !parseLine(text) )
-                {
-                    line = text;
-                }
-            }
-	}
-    }
+		messages << qMakePair( text, type );
 }
-
-void TextProtocol::setDevice(QIODevice* device)
-{
-    stream.setDevice(device);
-    connect ( device, &QIODevice::readyRead, this, &TextProtocol::readFromDevice );
-}
-
-QIODevice* TextProtocol::device() const
-{
-    return stream.device();
-}
-
-void TextProtocol::writeCheckMoves(const QString& text)
-{
-    Move m = Move(text);
-    if ( m.isValid() )
-    {
-      emit pieceMoved(m);
-    }
-    write(text);
-}
-
-void TextProtocol::write(const QString& text)
-{
-    qCDebug(LOG_KNIGHTS) << text;
-    stream << text << endl;
-}
-
-void TextProtocol::write(const char* text)
-{
-    qCDebug(LOG_KNIGHTS) << text;
-    stream << text << endl;
-}
-
-void TextProtocol::setConsole(ChatWidget* widget)
-{
-    m_console = widget;
-    foreach ( const ChatWidget::Message& message, messages )
-    {
-      m_console->addText ( message );
-    }
-    messages.clear();
-}
-
-ChatWidget* TextProtocol::console() const
-{
-    return m_console;
-}
-
-void TextProtocol::writeToConsole(const QString& text, ChatWidget::MessageType type)
-{
-    if ( m_console )
-    {
-      m_console->addText ( text, type );
-    }
-    else
-    {
-      messages << qMakePair( text, type );
-    }
-}
-
-
-
-
-
-
--- src/proto/textprotocol.h
+++ src/proto/textprotocol.h
@@ -1,66 +1,72 @@
-/*
-    This file is part of Knights, a chess board for KDE SC 4.
-    Copyright 2009,2010,2011  Miha Čančula <miha@noughmad.eu>
+/***************************************************************************
+    File                 : textprotocol.h
+    Project              : Knights
+    Description          : Base class for text protocols
+    --------------------------------------------------------------------
+    Copyright            : (C) 2017 by Alexander Semke (alexander.semke@web.de)
+    Copyright            : (C) 2009-2011 by Miha Čančula (miha@noughmad.eu)
 
-    This program is free software; you can redistribute it and/or
-    modify it under the terms of the GNU General Public License as
-    published by the Free Software Foundation; either version 2 of
-    the License or (at your option) version 3 or any later version
-    accepted by the membership of KDE e.V. (or its successor approved
-    by the membership of KDE e.V.), which shall act as a proxy
-    defined in Section 14 of version 3 of the license.
+ ***************************************************************************/
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+/***************************************************************************
+ *                                                                         *
+ *  This program is free software; you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation; either version 2 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the Free Software           *
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor,                    *
+ *   Boston, MA  02110-1301  USA                                           *
+ *                                                                         *
+ ***************************************************************************/
 
 #ifndef KNIGHTS_TEXTPROTOCOL_H
 #define KNIGHTS_TEXTPROTOCOL_H
 
-#include <QTextStream>
-#include <QList>
-
 #include <proto/protocol.h>
 #include "chatwidget.h"
 
+#include <QList>
+#include <QTextStream>
+
 namespace Knights {
 
-class TextProtocol : public Protocol
-{
-    Q_OBJECT
+class TextProtocol : public Protocol {
+	Q_OBJECT
 public:
-    TextProtocol(QObject* parent = 0);
-    virtual ~TextProtocol();
+	TextProtocol(QObject* parent = 0);
+	virtual ~TextProtocol();
 
 protected:
-    void setDevice(QIODevice* device);
-    QIODevice* device() const;
-    
-    void setConsole(ChatWidget* widget);
-    ChatWidget* console() const;
-    
-    void writeToConsole(const QString& text, ChatWidget::MessageType type);
+	void setDevice(QIODevice*);
+	QIODevice* device() const;
 
-    virtual bool parseLine(const QString& line) = 0;
-    virtual bool parseStub(const QString& line) = 0;
+	void setConsole(ChatWidget*);
+	ChatWidget* console() const;
+
+	void writeToConsole(const QString&, ChatWidget::MessageType);
+
+	virtual bool parseLine(const QString&) = 0;
+	virtual bool parseStub(const QString&) = 0;
 
 protected Q_SLOTS:
-    void readFromDevice();
-    void write(const QString& text);
-    void write(const char* text);
-
-    void writeCheckMoves(const QString& text);
+	void readFromDevice();
+	void write(const QString&);
+	void write(const char*);
+	void writeCheckMoves(const QString&);
 
 private:
-    QTextStream stream;
-    QPointer < ChatWidget > m_console;
-    QList < ChatWidget::Message > messages;
-    QString line;
+	QTextStream stream;
+	QPointer<ChatWidget> m_console;
+	QList<ChatWidget::Message> messages;
+	QString line;
 };
 
 }
