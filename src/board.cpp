KDE/knights

commit 8867833e00bdca2373a0b6ad9a4431d184501691

Clean up for making asan happy when we close apps

src/board.cpp
@@ -99,6 +99,7 @@ Board::~Board() {
 	qDeleteAll(m_grid);
 	qDeleteAll(m_tiles);
 	qDeleteAll(markers);
+	delete Manager::self()->rules();
 	delete renderer;
 }
 

commit d3e41cd8c5cf1274083497c249a4b72b3148abd8

Don't crash when new game is created. Regression introduced in 8867833e00bdca2373a0b6ad9a4431d184501691.
BUG: 405763
FIXED-IN: 19.04

src/board.cpp
@@ -99,8 +99,9 @@ Board::~Board() {
 	qDeleteAll(m_grid);
 	qDeleteAll(m_tiles);
 	qDeleteAll(markers);
-	delete Manager::self()->rules();
 	delete renderer;
+	//TODO: this crashes the application, s.a. BUG 405763
+	//delete Manager::self()->rules();
 }
 
 void Board::addPiece(PieceType type, Color color, const Pos& pos) {