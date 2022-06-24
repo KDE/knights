c950f9f8e3ed29df75daae8097d349601affa8bb
Laurent Montel <montel@kde.org>
2022-06-24 08:50:50 +0200

    Add missing QIODevice include

--- src/proto/textprotocol.cpp
+++ src/proto/textprotocol.cpp
@@ -25,6 +25,7 @@
  ***************************************************************************/
 #include "textprotocol.h"
 #include "knightsdebug.h"
+#include <QIODevice>
 
 using namespace Knights;
 
